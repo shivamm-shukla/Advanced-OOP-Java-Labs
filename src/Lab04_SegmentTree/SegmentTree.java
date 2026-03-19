a3d57ae16264c919495ff46b0ac0e38a782ca273
Shivam Shukla <shivamm.shukla00@gmail.com>
2026-03-19 11:52:30 +0530

    Segment Tree build implemented and tested

---
+++ src/Lab04_SegmentTree/SegmentTree.java
@@ -0,0 +1,71 @@
+package Lab04_SegmentTree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SegmentTree {
+
+    static class RangeSumST {
+
+        List<Integer> inputData;
+        List<Integer> STList;
+        int paddedSize;
+
+        RangeSumST(List<Integer> inputData) {
+            this.inputData = inputData;
+            this.paddedSize = getNextPower(inputData.size());
+
+            STList = new ArrayList<>(Collections.nCopies(2 * paddedSize, 0));
+
+            BuildST();
+        }
+
+        public int getNextPower(int n) {
+            int k = 1;
+            while (k < n) {
+                k = k * 2;
+            }
+            return k;
+        }
+
+        void BuildST() {
+            buildhelper(1);
+        }
+
+        void buildhelper(int index) {
+
+
+            if (index >= paddedSize) {
+
+                // padded nodes
+                if (index >= paddedSize + inputData.size()) {
+                    STList.set(index, 0);
+                }
+                // actual data
+                else {
+                    STList.set(index, inputData.get(index - paddedSize));
+                }
+                return;
+            }
+
+            // internal nodes
+            buildhelper(2 * index);
+            buildhelper(2 * index + 1);
+
+            STList.set(index,
+                    STList.get(2 * index) + STList.get(2 * index + 1));
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputData = new ArrayList<>(Arrays.asList(2, 4, 10, -7,8));
+
+        RangeSumST s = new RangeSumST(inputData);
+        System.out.println(s.STList);
+    }
+}
+
